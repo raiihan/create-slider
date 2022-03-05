5fb800df9254af8fd9fa258847c8c9e4983a799f
silvaan <s.siilvan@yahoo.com>
2022-03-05 13:21:54 +0600

    get HTML Element with javascript selector

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const imagesSection = document.querySelector('.images');
+const imagesGallery = document.querySelector('.gallery');
+const galleryHeader = document.querySelector('.gallery-header');
+const searchButton = document.getElementById('search-btn');
+const sliderButton = document.getElementById('create-slider');
+const sliderContainer = document.getElementById('sliders');
