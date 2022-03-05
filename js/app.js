fc131c5dbff4695cafb1f285e388622f11de005e
silvaan <s.siilvan@yahoo.com>
2022-03-05 15:09:14 +0600

    create slide section append innerHTLM

--- js/app.js
+++ js/app.js
@@ -24,14 +24,73 @@ searchButton.addEventListener('click', () => {
 })
 
 const displayImages = images => {
-    console.log(images)
     imagesGallery.textContent = '';
     // display the gallery header
     galleryHeader.style.display = 'flex'
     images.forEach(image => {
         let div = document.createElement('div');
         div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
-        div.innerHTML = `<img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`
+        div.innerHTML = `<img class="img-fluid img-thumbnail" onclick=selectImage(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`
         imagesGallery.appendChild(div);
     })
 }
+
+const selectImage = (event, img) => {
+    const imgElement = event.target;
+    imgElement.classList.add('added');
+    const imgItem = sliderImages.indexOf(img);
+    if (imgItem === -1) {
+        sliderImages.push(img);
+    }
+    else {
+        alert('Hey! Image allready added')
+    }
+}
+
+let slideIndex = 0;
+let timer;
+const createSlider = () => {
+    if (sliderImages.length < 2) {
+        alert('Select atleat 2 Image');
+        return;
+    }
+
+    // Create previous and next 
+    const prevNext = document.createElement('div');
+    prevNext.className = 'prev-next d-flex w-100 justify-content-between align-items-center';
+    prevNext.innerHTML = `
+    <span class="prev" onclick="changeItem(-1)"><i class="fas fa-chevron-left"></i></span>
+    <span class="next" onclick="changeItem(1)"><i class="fas fa-chevron-right"></i></span>
+  `;
+    sliderContainer.appendChild(prevNext);
+
+
+    const duration = document.getElementById('duration').value || 1000;
+
+    sliderImages.forEach(slide => {
+        let itemImg = document.createElement('div');
+        itemImg.className = "slider-item";
+        itemImg.innerHTML = `<img class="w-100"
+        src="${slide}"
+        alt="">`;
+        sliderContainer.appendChild(itemImg);
+    })
+
+    timer = setTimeout(() => {
+        slideIndex++;
+    }, duration)
+}
+
+const changeSlide = index => {
+    const items = document.querySelectorAll('.slider-item');
+    if (index < 0) {
+        slideIndex = items.length - 1;
+        index = slideIndex;
+    }
+
+}
+
+sliderButton.addEventListener('click', () => {
+    createSlider();
+    console.log('click')
+})
